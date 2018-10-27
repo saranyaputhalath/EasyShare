342abf8f21898b9b046d856e8ba3afacf5c1ef43
saranyaputhalath <saranyarajeev.p@gmail.com>
2018-10-27 13:58:30 -0700

    Added API full code

---
+++ BusinessLayerEasyShare/EntityEasyShare/GroupEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace EntityEasyShare
+{
+   public class GroupEntity
+    {
+        public int GroupId { get; set; }
+        public List<Users> Users { get; set; }
+    }
+}
---
+++ BusinessLayerEasyShare/EntityEasyShare/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityEasyShare
+{
+    public class UserEntity
+    {
+        public int MyProperty { get; set; }
+    }
+}
