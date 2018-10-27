2184e4de9766d7a418504b8fcd95f318106d0d3e
saranyaputhalath <saranyarajeev.p@gmail.com>
2018-10-27 15:19:51 -0700

    Get Category data from db

---
+++ BusinessLayerEasyShare/DALEasyShare/DataAccess.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALEasyShare
+{
+    public static class DataAccess
+    {
+        public static string ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=EasyShare;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static List<string> GetAllCategory()
+        {
+            List<string> category = new List<string>();
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("[dbo].[GetAllCategoryNames]", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    //cmd.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = txtFirstName.Text;
+                    //cmd.Parameters.Add("@LastName", SqlDbType.VarChar).Value = txtLastName.Text;
+
+                    con.Open();
+                    //cmd.ExecuteReader();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        var tb = new DataTable();
+                        tb.Load(dr);
+
+                        category = (from row in tb.AsEnumerable()
+                                          select row.Field<string>("Column Name")).ToList<string>();
+                    }
+                }
+            }
+            return category;
+        }
+
+        public static DataTable GetAllGroupsDetailByUserId(int userId)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("[dbo].[GetAllGroupsDetailByUserId]", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@userId", SqlDbType.VarChar).Value = userId;
+                    //cmd.Parameters.Add("@LastName", SqlDbType.VarChar).Value = txtLastName.Text;
+
+                    con.Open();
+                    //cmd.ExecuteReader();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        var tb = new DataTable();
+                        tb.Load(dr);
+                        return tb;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BusinessLayerEasyShare/DB/dbo.GetAllCategoryNames.sql
@@ -0,0 +1,18 @@
+﻿USE [EasyShare]
+GO
+
+/****** Object: SqlProcedure [dbo].[GetAllCategoryNames] Script Date: 10/27/2018 3:18:51 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP PROCEDURE [dbo].[GetAllCategoryNames];
+
+
+GO
+CREATE PROCEDURE [dbo].[GetAllCategoryNames]
+AS
+	SELECT * FROM [Category]
+RETURN 0
